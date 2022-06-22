c637a65284045fb5861daf94639cd04b9d3a6723
Lindsey Pollan <lpollan@live.com>
2022-06-21 20:37:26 -0500

    images show up

--- app.js
+++ app.js
@@ -1,19 +1,72 @@
 'use strict';
 //holds all product objects
 let allProducts = [];
-//holds all product names
-let maxClicks = 25; //sets click limit to 25 times
+//sets click limit to 25 times
+let maxClicks = 25; 
 let totalClicks = 0;
+//holds all product names
 let productNames = ['bag', 'banana', 'bathroom', 'boots'];
+
 //create constructor function that creates an object associated with each product
 function Product(name, path){
 this.timesShown=0;
 this.timesClicked=0;
 this.name=name;
 this.path=path;
-
 allProducts.push(this); //passes products through function
 }
+
+//algorithm is a step by step list of instructions to be executed
+//algorithms are functions
+function getRandomImage(){
+    return Math.floor(Math.random()*productNames.length); //math dot floor gives a whole number and math dot random gives me random value; return makes it do something
+    //run this on every product
+    //dot length returns random index number of names array
+}
+
+
+//global variables to represent div
+//get elements from html file
+const imageContainer = document.getElementById('image-container');
+const resultContainer = document.getElementById('results');
+let img_one = document.querySelector('#image-container img:first-child');
+let img_two = document.querySelector('#image-container img:nth-child(2)');
+let resultsButton = document.getElementById('results-button')
+
+//add event listener
+resultsButton.addEventListener('click', showResults)
+
+//below:instance variables -> object
+let bag = new Product('bag', './assets/bag.jpg')
+//or
+//let bag0 = new Product(productNames[0], './assets' + productNames[0] + '.jpg')
+let banana = new Product('banana', './assets/banana.jpg')
+
+//makes stuff
+function constructImages(){
+    //make an image for every name in name array
+   // for(let i=0;i<productNames.length;i++){
+   
+    //add src attrubute to images
+    img_one.setAttribute('src',bag.path);
+    img_two.setAttribute('src',banana.path);
+    //something wrong in above set attributs
+    img_one.setAttribute('alt',bag.name);
+    img_two.setAttribute('alt',banana.name);
+
+    img_one.addEventListener('click', trackClicks(bag));
+    img_two.addEventListener('click', trackClicks(banana));
+
+    
+
+    timesShown(bag) //put here bc this is where alt is
+}
+
+//make a function to randomly display images
+function displayRandomImage(){
+    //call the randomizer algorithm
+}
+
 //make method to track clicks
 function trackClicks(product){
     //whatever is in parameters is a placeholder
@@ -38,64 +91,28 @@ function timesShown(product){
         product.timesShown++
         console.log(product.timesShown)
     }else if
-    (product.name === img_two.alt)
+    (product.name === img_two.alt){
     console.log(product.name + ' is on the page')
     product.timesShown++
     console.log(product.timesShown)
-    else{
+    }else{
         console.log('there is no image here')
     }
 }
 
-//algorithm is a step by step list of instructions to be executed
-//algorithms are functions
-function getRandomImage(){
-    return Math.floor(Math.random()*productNames.length) //math dot floor gives a whole number and math dot random gives me random value; return makes it do something
-    //run this on every product
-    //dot length returns random index number of names array
-}
-
-//global variables to represent div
-//get elements from html file
-const imageContainer = document.getElementById('image-container');
-const resultContainer = document.getElementById('results');
-let img_one = document.querySelector('#image-container img:first-child');
-let img_two = document.querySelector('#image-container img:nth-child(2)');
-let resultsElement = document.getElementById('results-button')
-//above grabs elements from earlier
-//below:instance variables -> object
-let bag = new Product('bag', './assets/bag.png')
-//or
-//let bag0 = new Product(productNames[0], './assets' + productNames[0] + '.jpg')
-let banana = new Product('banana', './assets/banana.png')
-
-
-//makes stuff
-function constructImages(){
-    //make an image for every name in name array
-   // for(let i=0;i<productNames.length;i++){
-   
-    //add src attrubute to images
-    img_one.setAttribute('src',bag.path);
-    img_two.setAttribute('src',banana.path);
-    //something wrong in above set attributs
-    img_one.setAttribute('alt',bag.name);
-    img_two.setAttribute('alt',banana.name);
-
-    img_one.addEventListener('click', trackClicks(bag));
-    img_two.addEventListener('click', trackClicks(banana));
-
-    timesShown(bag) //put here bc this is where alt is
-}
-
+//display results on the results div
 function displayResults(productsArray){
 for (let i=0;i<productsArray.length;i++){
    let product =productsArray[i]
    //console.log(product)
-   console.log('this product was clicked ' + product.timesClicked + ' times')
-   console.log('this product was shown ' + product.timesClicked + ' times')
-   console.log('this product is called ' + product.timesClicked)
-   console.log('you can find this product ' + product.timesClicked)
+  let resultMessage = `this product was clicked ${product.timesClicked} times
+  this product was shown ${product.timesShown} times
+  this product is called ${product.name}
+  you can find this product at : ${product.path} !!!`
+  
+  let p = document.createElement('p');
+  p.textContent = resultMessage;
+  resultContainer.appendChild(p)
 } 
 //change above function based on pic on phone
 }
@@ -108,7 +125,7 @@ function showResults(){
 }
 
 //add event listeners
-resultsButton.addEventListener('click', showResults)
+
 //executable code
 
 constructImages()
