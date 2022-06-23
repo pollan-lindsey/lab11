9ea51dfc0d99d0323d092b914cf5057e5456a305
Lindsey Pollan <lpollan@live.com>
2022-06-23 10:52:57 -0500

    functional code minus randomizer

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ resultsButton.addEventListener('click', function () {
             },
             {
               label: '# of View',
-              data: timesSeen,
+              data: timesShown,
               backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
